7ea8b933fd1d6823b97cafef47409a7273cebae1
Apple <chanchai.chan@bumail.net>
2017-01-28 12:49:11 +0700

    9.Creating hazards

---
+++ Assets/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+	//use this for initalization
+	void Start() {
+		
+	}
+	void OntriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary") {
+			return;}
+		Destroy (gameObject);//ลบตัวเอง
+		Destroy (other.gameObject);// ลบของที่ชน
+	}
+}
